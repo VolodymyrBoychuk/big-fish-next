d0a0dc2ff40cc7d8ba74f08cb2df0427bade28a0
VolodymyrBoychuk <vladimirboychuk@gmail.com>
2025-11-29 14:30:26 +0200

    Create page.tsx
    add Privacy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,58 @@
+export default function Privacy() {
+  return (
+    <section className="relative bg-[#0c1650] h-screen overflow-hidden pt-16">
+      <div className="max-w-6xl mx-auto relative py-12 md:py-20 px-4 text-white">
+        <strong>Privacy Policy</strong>
+        <p>
+          This privacy policy applies to the CarpTracker app (hereby referred to
+          as "Application") for mobile devices that was created by Vionum
+          (hereby referred to as "Service Provider") as a Free service. This
+          service is intended for use "AS IS".
+        </p>
+
+        <br />
+
+        <strong>
+          What information does the Application obtain and how is it used?
+        </strong>
+        <p>
+          The Application does not obtain any information when you download and
+          use it. Registration is not required to use the Application.
+        </p>
+
+        <br />
+
+        <strong>
+          Does the Application collect precise real time location information of
+          the device?
+        </strong>
+        <p>
+          This Application does not collect precise information about the
+          location of your mobile device.
+        </p>
+
+        <br />
+
+        <strong>
+          Do third parties see and/or have access to information obtained by the
+          Application?
+        </strong>
+        <p>
+          Since the Application does not collect any information, no data is
+          shared with third parties.
+        </p>
+
+        <br />
+
+        <strong>Contact Us</strong>
+        <p>
+          If you have any questions regarding privacy while using the
+          Application, please contact the Service Provider via email at
+          carptracker.app@gmail.com.
+        </p>
+
+        <hr />
+      </div>
+    </section>
+  );
+}
